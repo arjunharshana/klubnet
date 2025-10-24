2c9a07d00b6886dc83f34b0e8bdc3c4e8facf77a
arjunharshana <arjunharshana11@gmail.com>
2025-10-24 13:13:26 +0530

    Bug fixes

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,12 +2,14 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const cookieParser = require("cookie-parser");
-
+const helmet = require("helmet");
 const userRoutes = require("./routes/userRoutes");
 
 dotenv.config();
 
 const app = express();
+
+app.use(helmet());
 
 app.use(
   cors({
@@ -24,5 +26,17 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/users", userRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).json({ message: "Route not found" });
+
+  next();
+});
+
+app.use((err, req, res, next) => {
+  let statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+  console.error(err);
+  res.status(statusCode).json({ message: "Internal Server Error" });
+});
 
 module.exports = app;
--- backend/src/middleware/authMiddleware.js
+++ backend/src/middleware/authMiddleware.js
@@ -6,21 +6,14 @@ const authMiddleware = async (req, res, next) => {
   const token = req.cookies.token;
 
   if (token) {
-    try {
-      const decoded = jwt.verify(token, process.env.JWT_SECRET);
-      req.user = await User.findById(decoded.id).select("-password");
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = await User.findById(decoded.id).select("-password");
 
-      if (!req.user) {
-        return res
-          .status(401)
-          .json({ message: "Unauthorized: User not found" });
-      }
-
-      next();
-    } catch (error) {
-      console.error("Auth Middleware Error:", error);
-      return res.status(401).json({ message: "Unauthorized: Invalid token" });
+    if (!req.user) {
+      return res.status(401).json({ message: "Unauthorized: User not found" });
     }
+
+    next();
   } else {
     return res.status(401).json({ message: "Unauthorized: No token provided" });
   }
