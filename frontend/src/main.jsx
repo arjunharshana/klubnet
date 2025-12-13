48b8c0917b1a746816d75a028a3d1a8e87349a3d
arjunharshana <arjunharshana11@gmail.com>
2025-12-13 22:08:05 +0530

    Bug fix

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import { createRoot } from "react-dom/client";
 import { AuthProvider } from "./context/AuthContext.jsx";
 
 import "./index.css";
@@ -12,7 +11,9 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <AuthProvider>
+        <App />
+      </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
