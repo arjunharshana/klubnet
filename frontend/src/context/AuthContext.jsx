2b6723649367c3db1065ce9b8cc7379c21338299
arjunharshana <arjunharshana11@gmail.com>
2025-12-13 21:53:10 +0530

    Create context for user login

---
+++ frontend/src/context/AuthContext.jsx
@@ -0,0 +1,56 @@
+import { createContext, useState, useEffect, useContext } from "react";
+import axios from "axios";
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const checkUserLoggedIn = async () => {
+      try {
+        const apiUri = import.meta.env.VITE_API_URI;
+        const response = await fetch(`${apiUri}/api/users/profile`, {
+          credentials: "include",
+        });
+        if (response.ok) {
+          const data = await response.json();
+          setUser(data);
+        }
+        setLoading(false);
+      } catch (error) {
+        console.error("Failed to fetch user:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    checkUserLoggedIn();
+  }, []);
+
+  const login = (userData) => {
+    setUser(userData);
+  };
+
+  const logout = async () => {
+    try {
+      const apiUri = import.meta.env.VITE_API_URI;
+      await axios.post(
+        `${apiUri}/api/users/logout`,
+        {},
+        { withCredentials: true }
+      );
+    } catch (err) {
+      console.error("Logout failed:", err);
+    }
+
+    setUser(null);
+  };
+
+  return (
+    <AuthContext.Provider value={{ user, login, logout, loading }}>
+      {!loading && children}
+    </AuthContext.Provider>
+  );
+};
