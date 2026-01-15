1de4852b045e1ef8e7073c7e9ab1c6d7e6ced6c3
arjunharshana <arjunharshana11@gmail.com>
2026-01-15 10:58:43 +0530

    Add CRUD for Clubs

--- backend/src/app.js
+++ backend/src/app.js
@@ -27,6 +27,7 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/users", userRoutes);
+app.use("/api/clubs", clubRoutes);
 
 app.use((req, res, next) => {
   res.status(404).json({ message: "Route not found" });
