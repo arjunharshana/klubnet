a6fcb5002dbe548d4927a1e0a6948493a028670b
arjunharshana <arjunharshana11@gmail.com>
2026-01-26 20:56:21 +0530

    Create event model

---
+++ backend/src/models/event.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+
+const eventSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: [true, "Title is required"],
+      trim: true,
+    },
+    description: {
+      type: String,
+      required: [true, "Description is required"],
+      trim: true,
+    },
+    date: {
+      type: Date,
+      required: [true, "Date is required"],
+    },
+    location: {
+      type: String,
+      required: [true, "Location is required"],
+      trim: true,
+    },
+    club: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Club",
+      required: true,
+    },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    attendees: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  { timestamps: true },
+);
+
+module.exports = mongoose.model("Event", eventSchema);
